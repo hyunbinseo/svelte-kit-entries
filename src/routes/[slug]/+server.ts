55d04a9996800b0bdc78350833057c76b039546c
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2023-05-16 14:50:45 +0900

    demo: /output/prerendered not generated

--- src/routes/[slug]/+server.ts
+++ src/routes/[slug]/+server.ts
@@ -1,7 +1,7 @@
 import { json } from '@sveltejs/kit';
-import type { EntryGenerator, RequestHandler } from './$types';
+import type { RequestHandler } from './$types';
 
-export const prerender = true;
-export const entries = (() => [{ slug: '2' }]) satisfies EntryGenerator;
+// export const prerender = true;
+// export const entries = (() => [{ slug: '2' }]) satisfies EntryGenerator;
 
 export const GET = (({ params }) => json(params)) satisfies RequestHandler;
