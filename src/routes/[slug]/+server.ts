78006d10767927843adb59c66845a2deebfb8364
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2023-05-16 14:51:07 +0900

    demo: only /pages/1.html is pre-rendered

--- src/routes/[slug]/+server.ts
+++
@@ -1,7 +0,0 @@
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-
-// export const prerender = true;
-// export const entries = (() => [{ slug: '2' }]) satisfies EntryGenerator;
-
-export const GET = (({ params }) => json(params)) satisfies RequestHandler;
